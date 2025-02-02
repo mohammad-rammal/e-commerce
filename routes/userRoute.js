6262491c7690b746e26614d2cf86a0fb3dd36c5e
mohammad-rammal <mohammad.rammal@hotmail.com>
2025-02-02 16:46:48 +0200

    User Validator

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -8,10 +8,20 @@ const {
     updateUser,
     deleteUser,
 } = require('../controllers/userController');
+const {
+    createUserValidator,
+    deleteUserValidator,
+    updateUserValidator,
+    getUserValidator,
+} = require('../utils/validators/userValidator');
 
 const router = express.Router();
 
-router.route('/').get(getUsers).post(uploadUserImage, resizeImage, createUser);
-router.route('/:id').get(getUser).put(uploadUserImage, resizeImage, updateUser).delete(deleteUser);
+router.route('/').get(getUsers).post(uploadUserImage, resizeImage, createUserValidator, createUser);
+router
+    .route('/:id')
+    .get(getUserValidator, getUser)
+    .put(uploadUserImage, resizeImage, updateUserValidator, updateUser)
+    .delete(deleteUserValidator, deleteUser);
 
 module.exports = router;
--- utils/validators/userValidator.js
+++ utils/validators/userValidator.js
@@ -1,6 +1,7 @@
 const slugify = require('slugify');
 const {check, body} = require('express-validator');
 const validatorMiddleware = require('../../middlewares/validatorMiddleware');
+const UserModel = require('../../models/userModel');
 
 exports.getUserValidator = [check('id').isMongoId().withMessage('Invalid User ID Format'), validatorMiddleware];
 
@@ -8,12 +9,46 @@ exports.createUserValidator = [
     check('name')
         .notEmpty()
         .withMessage('User required!')
-        .isLength({min: 6})
+        .isLength({min: 2})
         .withMessage('Must be more than 2 chars')
         .custom((val, {req}) => {
             req.body.slug = slugify(val);
             return true;
         }),
+
+    check('email')
+        .notEmpty()
+        .withMessage('Email required!')
+        .isEmail()
+        .withMessage('Invalid email address!')
+        .custom((val) =>
+            UserModel.findOne({email: val}).then((user) => {
+                if (user) {
+                    return Promise.reject(new Error('Email already exists!'));
+                }
+            }),
+        ),
+
+    check('password')
+        .notEmpty()
+        .withMessage('PAssword is required!')
+        .isLength({min: 6})
+        .withMessage('Must be more than 6 chars')
+        .custom((password, {req}) => {
+            if (password !== req.body.passwordConfirm) {
+                throw new Error('Password confirmation incorrect');
+            }
+            return true;
+        }),
+
+    check('passwordConfirm').notEmpty().withMessage('Password confirmation required!'),
+
+    check('phone').optional().isMobilePhone(['ar-LB', 'en-US']).withMessage('Invalid phone number!'),
+
+    check('profileImg').optional(),
+
+    check('role').optional(),
+
     validatorMiddleware,
 ];
 
